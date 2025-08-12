7929aa76eb3a02f37c83d6d5028cdb9dd3151687
kalyantalbothula <kalyan6166@gmail.com>
2025-08-12 11:11:30 +1000

    deleted old backend and tfvars files, updated provider

--- dev/backend.tf
+++
@@ -1,6 +0,0 @@
-    bucket = "s3-kalyan-bucket-dev"  
-    key    = "tfvars-demo"    
-    region = "us-east-1"
-    encrypt = true
-    use_lockfile = true 
-    #dynamodb_table = "84s-remote-state"  
--- tfvars/provider.tf
+++ tfvars/provider.tf
@@ -12,12 +12,12 @@ terraform {
 # 🔍  terraform remote state s3 bucket
 
   backend "s3" {        # <-- backend is ntg but state ani ardham.
-    bucket = "s3-kalyan-bucket"   # <---
-    key    = "tfvars-demo"    # <--- giving key as "remote-state-demo"
-    region = "us-east-1"
-    encrypt = true
-    use_lockfile = true 
-    #dynamodb_table = "84s-remote-state"  # <-- mention table name
+    # bucket = "s3-kalyan-bucket"   # <---
+    # key    = "tfvars-demo"    # <--- giving key as "remote-state-demo"
+    # region = "us-east-1"
+    # encrypt = true
+    # use_lockfile = true 
+    # #dynamodb_table = "84s-remote-state"  # <-- mention table name
   }
 }
 
